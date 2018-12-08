d343d8bbe2b4442daa53f4dc7b5fa3228a658164
stellalee <stila917@gmail.com>
2018-12-08 23:50:19 +0000

    mocha and chai are added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// export? to be called at test
+// 1. Unit-text  functions are separated from component
+export const doIncrement = (prev) => ({
+    counter: prev.counter + 1
+});
+export const doDecrement = (prev) => ({
+    counter: prev.counter - 1
+});
 
+
+
+// 2.Integration test:  add a children component
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+        this.onIncrement = this.onIncrement.bind(this);
+        this.onDecrement = this.onDecrement.bind(this);
+
+    }
+
+    onIncrement() {
+        this.setState(doIncrement);
+    }
+
+    onDecrement() {
+        this.setState(doDecrement);
+    }
+
+    render() {
+        const {counter} = this.state;
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Counter</h1>
+                    <p>{counter}</p>
+                </header>
+                <button type='button'
+                        onClick={this.onIncrement}>+</button>
+                <button type='button'
+                        onClick={this.onDecrement}>-</button>
+            </div>
+        );
+    }
+}
+export const Counter =({counter}) => (<p>{counter}</p>)
 export default App;
